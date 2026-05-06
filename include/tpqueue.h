a6301947d535650feaf0afb849e16793c3469e62
maksimkk6 <konnovmaks@bk.ru>
2026-05-06 20:34:28 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,63 +4,65 @@
 
 template<typename T>
 class TPQueue {
-private:
-    struct Node {
-        T data;
-        Node* next;
-        Node(const T& value) : data(value), next(nullptr) {}
-    };
-    Node* head;
-    Node* tail;
-public:
-    TPQueue() : head(nullptr), tail(nullptr) {}
-    ~TPQueue() {
-        while (head != nullptr) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+  };
+  Node* head;
+  Node* tail;
+
+ public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
+  ~TPQueue() {
+    while (head != nullptr) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
     }
-    T pop() {
-        T result = head->data;
-        Node* temp = head;
-        head = head->next;
-        if (head == nullptr) {
-            tail = nullptr;
-        }
-        delete temp;
-        return result;
+  }
+  T pop() {
+    T result = head->data;
+    Node* temp = head;
+    head = head->next;
+    if (head == nullptr) {
+      tail = nullptr;
     }
-    void push(const T& value) {
-        Node* newNode = new Node(value);
-        if (head == nullptr) {
-            head = newNode;
-            tail = newNode;
-            return;
-        }
-        if (value.prior > head->data.prior) {
-            newNode->next = head;
-            head = newNode;
-            return;
-        }
-        Node* current = head;
-        while (current->next != nullptr && current->next->data.prior >= value.prior) {
-            current = current->next;
-        }
-        newNode->next = current->next;
-        current->next = newNode;
-        if (newNode->next == nullptr) {
-            tail = newNode;
-        }
+    delete temp;
+    return result;
+  }
+  void push(const T& value) {
+    Node* newNode = new Node(value);
+    if (head == nullptr) {
+      head = newNode;
+      tail = newNode;
+      return;
     }
-    bool empty() const {
-        return head == nullptr;
+    if (value.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+      return;
     }
+    Node* current = head;
+    while (current->next != nullptr &&
+           current->next->data.prior >= value.prior) {
+      current = current->next;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+    if (newNode->next == nullptr) {
+      tail = newNode;
+    }
+  }
+  bool empty() const {
+    return head == nullptr;
+  }
 };
 
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
